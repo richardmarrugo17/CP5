8bf20d3c224fb6f74e693aec2234c7cb9138a8a8
richardmarrugo17 <richard17_m@hotmail.com>
2022-01-24 17:31:35 -0500

    correciones

--- Javascript/login.js
+++ Javascript/login.js
@@ -2,7 +2,7 @@ var myModal = new bootstrap.Modal(document.getElementById('divModal'));
 
 function validarLogin(){
     usr = document.getElementById("usuario");
-    pwd = document.getElementById("contraseña");
+    pwd = document.getElementById("password");
 
     if (usr.value == "admin" && pwd.value == "12345"){
         location.href = "dashboard.html";
